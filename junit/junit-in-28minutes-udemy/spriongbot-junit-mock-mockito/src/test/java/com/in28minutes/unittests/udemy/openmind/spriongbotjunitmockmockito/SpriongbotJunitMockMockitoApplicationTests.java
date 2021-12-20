c755a60f05e3022e0660c0591914a104c5f6ff58
mrebouças <marceloreboucas10@gmail.com>
2021-12-20 05:53:53 -0300

    Changes e share a junit project

---
+++ junit/junit-in-28minutes-udemy/spriongbot-junit-mock-mockito/src/test/java/com/in28minutes/unittests/udemy/openmind/spriongbotjunitmockmockito/SpriongbotJunitMockMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.unittests.udemy.openmind.spriongbotjunitmockmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpriongbotJunitMockMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
